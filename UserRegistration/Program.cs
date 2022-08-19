2554a3b43f4db29e0197c4f18a7e08ce3a245450
praveen dodla <dodlapraveen09@gmail.com>
2022-08-19 20:56:43 +0530

    Valid Email

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -6,22 +6,24 @@ namespace UserRegistration
     class Program
     {
         public static string REGEX_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}";
+        public static string EMAIL_PATTERN = "^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*(\\@)[a-zA-Z0-9]+(\\.)[a-z]{2,3}[.(a-z)]*$";
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome_To_UserRegistration_RegeEx");
             FirstNameVal();
             LastNameVal();
+            EmailValidation();
         }
         public static void FirstNameVal()
-        {
-            Regex rg = new Regex(REGEX_PATTERN);
-            Console.Write("Enter First Name: ");
+        {           
+            Regex rg = new Regex(REGEX_PATTERN);            
+            Console.Write("Enter First Name: ");            
             string first_name = Console.ReadLine();
             bool validate = rg.IsMatch(first_name);
             if (validate)
                 Console.WriteLine("Name is Valid : " + first_name);
             else
-                Console.WriteLine("Invalid FirstName");
+                Console.WriteLine("Invalid FirstName");           
         }
         public static void LastNameVal()
         {
@@ -33,6 +35,17 @@ namespace UserRegistration
                 Console.WriteLine("Name is Valid : " + last_name);
             else
                 Console.WriteLine("Invalid LastName");
+        }
+        public static void EmailValidation()
+        {
+            Regex rg = new Regex(EMAIL_PATTERN);
+            Console.Write("Enter E-mail: ");
+            string email = Console.ReadLine();
+            bool validate = rg.IsMatch(email);
+            if (validate)
+                Console.WriteLine("Email is valid : " + email);
+            else
+                Console.WriteLine("Invalid E-mail.");
         }
     }
 }
